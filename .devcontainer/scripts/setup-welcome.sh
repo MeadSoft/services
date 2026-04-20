f53c0ed1032948e4dfa0de684ede26c2c4655ef5
Xerner <kennethmead784@gmail.com>
2026-04-20 16:44:44 +0000

    updated scripts

--- .devcontainer/scripts/setup-welcome.sh
+++ .devcontainer/scripts/setup-welcome.sh
@@ -1,8 +1,11 @@
 PRIMARY_COLOR=$COLOR_GRAY
 SECONDARY_COLOR=$COLOR_NC
 INFO_COLOR=$COLOR_CYAN
+SSH_GITHUB_STATUS="unknown"
 
-WELCOME_INFO="${SECONDARY_COLOR}
+create_welcome_message() {
+    local welcome_info
+    welcome_info="${SECONDARY_COLOR}
 ██     ██ ███████ ██       ██████  ██████  ███    ███ ███████
 ██     ██ ██      ██      ██      ██    ██ ████  ████ ██     
 ██  █  ██ █████   ██      ██      ██    ██ ██ ████ ██ █████  
@@ -12,34 +15,37 @@ ${PRIMARY_COLOR}
 to the MeadSoft's ${INFO_COLOR}$(basename "$WORKSPACE_FOLDER")${PRIMARY_COLOR} mono-repository.
 This mono-repository contains several projects, libraries, and tools
 
-Workspace Folder       ${SECONDARY_COLOR}$WORKSPACE_FOLDER
+Workspace Folder      ${SECONDARY_COLOR}$WORKSPACE_FOLDER
 
 ${INFO_COLOR}Library Version(s)${PRIMARY_COLOR}
-nvm                 ${SECONDARY_COLOR}$(nvm --version)${PRIMARY_COLOR}
-Node                ${SECONDARY_COLOR}$(node --version)${PRIMARY_COLOR}
-pnpm                ${SECONDARY_COLOR}$(pnpm --version)${PRIMARY_COLOR}
-Angular             ${SECONDARY_COLOR}$(ng --version)${PRIMARY_COLOR}
-gcloud              ${SECONDARY_COLOR}$(gcloud --version | head -n 1)${PRIMARY_COLOR}
-Python              ${SECONDARY_COLOR}$(python --version 2>&1)${PRIMARY_COLOR}
-Terraform           ${SECONDARY_COLOR}$(terraform --version | head -n 1)${PRIMARY_COLOR}
+nvm                   ${SECONDARY_COLOR}$(nvm --version)${PRIMARY_COLOR}
+Node                  ${SECONDARY_COLOR}$(node --version)${PRIMARY_COLOR}
+pnpm                  ${SECONDARY_COLOR}$(pnpm --version)${PRIMARY_COLOR}
+Angular               ${SECONDARY_COLOR}$(ng --version)${PRIMARY_COLOR}
+gcloud                ${SECONDARY_COLOR}$(gcloud --version | head -n 1)${PRIMARY_COLOR}
+Python                ${SECONDARY_COLOR}$(python3 --version 2>&1)${PRIMARY_COLOR}
+Terraform             ${SECONDARY_COLOR}$(terraform --version | head -n 1)${PRIMARY_COLOR}
 
 ${INFO_COLOR}Git${PRIMARY_COLOR}
-Username            ${SECONDARY_COLOR}$(git config user.name)${PRIMARY_COLOR}
-Email               ${SECONDARY_COLOR}$(git config user.email)${PRIMARY_COLOR}
+Username              ${SECONDARY_COLOR}$(git config user.name)${PRIMARY_COLOR}
+Email                 ${SECONDARY_COLOR}$(git config user.email)${PRIMARY_COLOR}
 "
+    SSH_GITHUB_STATUS="$(ssh -T git@github.com 2>&1 || true)"
 
-SSH_GITHUB_STATUS="$(ssh -T git@github.com 2>&1 || true)"
-
-if [[ -n "$SSH_GITHUB_STATUS" ]]; then
-WELCOME_INFO+="
+    if [[ -n "$SSH_GITHUB_STATUS" ]]; then
+    welcome_info+="
 ${INFO_COLOR}SSH${PRIMARY_COLOR}
-Github Connection   ${SECONDARY_COLOR}$SSH_GITHUB_STATUS${PRIMARY_COLOR}
+Github Connection     ${SECONDARY_COLOR}$SSH_GITHUB_STATUS${PRIMARY_COLOR}
 "
-fi
+    fi
 
-WELCOME_INFO+="
+    welcome_info+="
 to see this info again, use the ${INFO_COLOR}welcome${PRIMARY_COLOR} command${COLOR_NC}
 "
+    echo "$welcome_info"
+}
+
+WELCOME_INFO="$(create_welcome_message)\n"
 
 alias welcome='printf "$WELCOME_INFO"'
 welcome
---
+++ scripts/restart-ssh-agent.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+eval "$(ssh-agent -k)" 2>/dev/null; eval "$(ssh-agent -s)"
